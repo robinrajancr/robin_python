6f11ec33e3d9902660522a55da1c95b553570ffa
Robin CR <robin.cr@gmail.com>
2018-12-10 09:56:22 +0530

    function programs

---
+++ calfun.py
@@ -0,0 +1,4 @@
+#Calculation Function 
+def my_function(x,y,z):
+ return 5*x
+ print (my_function(3))
---
+++ function.py
@@ -0,0 +1,13 @@
+#Syntax for Function 
+#     def function name()
+#      {
+#              codeline 
+#
+#              } 
+
+def username (jname):
+ print("username" + jname )
+
+username (" emailId")
+username (" robin")
+username (" Robin.CR")
