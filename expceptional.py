ed4ed378cc1d005f60df3ad384badeaeb862c093
Robin CR <robin.cr@gmail.com>
2019-01-08 09:50:05 +0530

    java and python

---
+++ expceptional.py
@@ -0,0 +1,12 @@
+try:
+ a= int(input("Enter the number"))
+ b= int(input("Enter the number"))
+#    int c
+ c=a/b 
+ print("a/b=%d"%c)
+except Exception:
+ print ("cant divide by Zero!")
+else: 
+ print ("else block")
+
+
