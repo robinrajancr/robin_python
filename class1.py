71c3a8e18f401a7c76511bb926ea453dbb6786d2
Robin CR <robin.cr@gmail.com>
2019-01-08 10:56:23 +0530

    class

---
+++ class1.py
@@ -0,0 +1,10 @@
+class person:
+ def __init__(self, name, age):
+  self.name = name
+  self.age = age
+ def display(self):
+     print ("Hello this is my name is " + self.name,self.age)
+p1 = person("Robin","37")
+p1.display()
+#p2 = person("Robin","37")
+#p2.display()
